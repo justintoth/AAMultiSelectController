56dbfabbd964f4a069620af651ee4b29623c9b22
Jonathan Mayunga <mayunga.j@gmail.com>
2019-01-17 17:26:35 +0200

    Update AAMultiSelectViewController.m

--- AAMultiSelectController/Classes/AAMultiSelectViewController.m
+++ AAMultiSelectController/Classes/AAMultiSelectViewController.m
@@ -13,6 +13,7 @@
 #import "AAMultiSelectModel.h"
 
 #define AA_SAFE_BLOCK_CALL(block, ...) block ? block(__VA_ARGS__) : nil
+#define AA_CANCEL_BLOCK_CALL(block, ...) block ? block(__VA_ARGS__) : nil
 #define AA_SELECT_BLOCK_CALL(block, ...) block ? block(__VA_ARGS__) : nil
 #define WeakObj(o) autoreleasepool{} __weak typeof(o) weak##o = o;
 #define UIColorFromHexWithAlpha(hexValue,a) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:a]
@@ -310,7 +311,8 @@ static NSInteger const separatorBackgroundColor        = 0XDCDCDC;
 
 - (void)cancelButtonTapped {
     [self.popupView dismiss:YES];
-    AA_SAFE_BLOCK_CALL(self.cancelBlock, nil);
+    
+    AA_CANCEL_BLOCK_CALL(self.cancelBlock, false);
 }
 
 #pragma mark - Helper
