387e4bef4dc2bc860e303812ca97004ea31695b5
Jonathan Mayunga <mayunga.j@gmail.com>
2019-01-18 14:44:12 +0200

    Update AAMultiSelectViewController.m

--- AAMultiSelectController/Classes/AAMultiSelectViewController.m
+++ AAMultiSelectController/Classes/AAMultiSelectViewController.m
@@ -292,8 +292,10 @@ static NSInteger const separatorBackgroundColor        = 0XDCDCDC;
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     AAMultiSelectModel *selectModel = self.tempDataArray[indexPath.row];
     selectModel.isSelected          = !selectModel.isSelected;
-    [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
-    AA_SELECT_BLOCK_CALL(self.selectedBlock, selectModel);
+    if(selectModel.isSelected){
+        [tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+        AA_SELECT_BLOCK_CALL(self.selectedBlock, selectModel);
+    }
 }
 
 #pragma mark - Events
